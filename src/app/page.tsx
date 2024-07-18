15a5f945356b7a0f0d2cdf42a0c8801d6efd5870
Joao Matheus <joaomatheusramos@gmail.com>
2024-07-18 01:42:40 -0300

    ajustando erro em chat page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 
-import {Chat} from '@/pages/chat/main'
+// import {Chat} from '@/pages/chat/main'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <Chat/>
+      {/* <Chat/> */}
+     <h1>Hello World</h1>
     </main>
   );
 }
